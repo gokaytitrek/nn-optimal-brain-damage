adca74a9a8992bb6c6f35af4b56fb0095113aee3
Gokay Titrek <gokaytitrek@hotmail.com>
2014-11-07 17:53:23 +0200

    - XOR weight ve bias degerleri manual olarak eklendi.

--- skynet/src/skynet/SkyNet.java
+++ skynet/src/skynet/SkyNet.java
@@ -9,6 +9,19 @@ public class SkyNet {
         Layer l2 = new Layer(1,0);
         nn.addLayer(l2);
         
+        nn.layers[0].getPerceptron(0).weights[0]=1;
+        nn.layers[0].getPerceptron(1).weights[0]=1;
+        nn.layers[0].getPerceptron(0).weights[1]=1;
+        nn.layers[0].getPerceptron(1).weights[1]=1;        
+        
+        nn.layers[1].getPerceptron(0).weights[0]=-1;
+        nn.layers[1].getPerceptron(1).weights[0]=1;
+        nn.layers[1].getPerceptron(0).biasWeight = 1.5;
+        nn.layers[1].getPerceptron(1).biasWeight = 0.5;
+        
+        nn.layers[2].getPerceptron(0).biasWeight= 0.5;
+        
+        
         double [] input = {1,1};
         double [] output = nn.run(input);
     }
